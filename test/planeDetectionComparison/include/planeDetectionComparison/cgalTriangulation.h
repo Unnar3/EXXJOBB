54ce25206d83bdbc8163f40ede97c22106b70866
Unnar Axelsson <unnar@kth.se>
2016-02-18 16:53:18 +0100

    created a mesh using delaunay

--- test/planeDetectionComparison/include/planeDetectionComparison/cgalTriangulation.h
+++ test/planeDetectionComparison/include/planeDetectionComparison/cgalTriangulation.h
@@ -11,6 +11,8 @@
 #include <CGAL/Triangulation_vertex_base_with_info_2.h>
 #include <CGAL/Spatial_sort_traits_adapter_2.h>
 
+#include <PointTypes/surfel_type.h>
+#include <typedefs/typedef.h>
 #include <Eigen/Dense>
 #include <vector>
 
@@ -25,6 +27,15 @@ typedef CGAL::Exact_predicates_tag                                          Itag
 typedef CGAL::Constrained_Delaunay_triangulation_2<Kernel, TDS, Itag>       CDT;
 typedef CGAL::Spatial_sort_traits_adapter_2<Kernel,Point*>                  Search_traits;
 
+using PointT = pcl::PointXYZRGB;
+using PointCloudT = pcl::PointCloud<PointT>;
+using PointNT = pcl::PointNormal;
+using PointNCloudT = pcl::PointCloud<PointNT>;
+using NormalT = pcl::Normal;
+using NormalCloudT = pcl::PointCloud<NormalT>;
+using SurfelT = SurfelType;
+using SurfelCloudT = pcl::PointCloud<SurfelT>;
+
 
 template <class InputIterator>
 void insert_with_info(CDT& cdt, InputIterator first,InputIterator last);
@@ -34,11 +45,12 @@ void constrainedDelaunayTriangulation(  std::vector<Point>                      
                                         std::vector<Point>                      constraines,
                                         std::vector<std::vector<unsigned int> > &idx);
 
-template <typename PointT>
-void pclPlaneToCGAL(    pcl::PointCloud<PointT>::Ptr plane,
-                        pcl::PointCloud<PointT>::Ptr boundary,
-                        pcl::ModelCoefficients::Ptr coeff
-                        std::vector<std::vector<unsigned int> > &idx);
+template <class T>
+void pclPlaneToCGAL(    typename pcl::PointCloud<T>::Ptr    plane,
+                        typename pcl::PointCloud<T>::Ptr    boundary,
+                        pcl::ModelCoefficients::Ptr         coeff,
+                        std::vector<Point >               & plane_2d,
+                        std::vector<Point >               & boundary_2d);
 
 
 /////////////////////////////////////////////////////////////
@@ -46,27 +58,60 @@ void pclPlaneToCGAL(    pcl::PointCloud<PointT>::Ptr plane,
 /////////////////////////////////////////////////////////////
 
 // Takes in PointCloud where all points are lying on a plane and projects that plane to 2d.
-template <typename PointT>
-void pclPlaneToCGAL(    pcl::PointCloud<PointT>::Ptr plane,
-                        pcl::PointCloud<PointT>::Ptr boundary,
-                        pcl::ModelCoefficients::Ptr coeff,
-                        std::vector<Point > &plane_2d,
-                        std::vector<Point > &boundary_2d){
+template <class T>
+void pclPlaneToCGAL(    typename pcl::PointCloud<T>::Ptr    plane,
+                        typename pcl::PointCloud<T>::Ptr    boundary,
+                        pcl::ModelCoefficients::Ptr         coeff,
+                        std::vector<Point >               & plane_2d,
+                        std::vector<Point >               & boundary_2d){
+
+    // Make sure 2d vectors are empty and efficient.
+    if(plane_2d.size() != 0){
+        plane_2d.clear();
+        plane_2d.reserve(plane->points.size());
+    }
+    if(boundary_2d.size() != 0){
+        boundary_2d.clear();
+        boundary_2d.reserve(boundary->points.size());
+    }
 
     // Create the normal vector
-    Eigen::Vector3f normal;
-    for (size_t i = 0; i < normal.size(); i++) {
-        normal[i] = coeff->values[i];
+    Eigen::Vector3f N;
+    for (size_t i = 0; i < N.size(); i++) {
+        N[i] = coeff->values[i];
     }
-    normal = normal/normal.norm();
+    N = N / N.norm();
 
-    // Create arbitrary vector U not parallell to normal to find a new X/Y axis
+    // Create arbitrary vector U not parallell to N to find a new X/Y axis
     Eigen::Vector3f U(1,0,0);
-    if( std::abs(normal.dot(U)) > 0.8 ){
-        // U to similar to normal change it to [0 1 0].
+    if( std::abs(N.dot(U)) > 0.8 ){
+        // U to similar to N change it to [0 1 0].
         U[0] = 0; U[1] = 1;
     }
 
+    // create the new x y axis orthogonal to N.
+    Eigen::Vector3f X = U - (U.dot(N) * N);
+    Eigen::Vector3f Y = N.cross(X);
+
+    // normalize the axis
+    X = X / X.norm();
+    Y = Y / Y.norm();
+
+    // Loop through all points and project them to new 2d coordinate system
+    Eigen::Vector3f point;
+    for(auto p : plane->points){
+        point[0] = p.x;
+        point[1] = p.y;
+        point[2] = p.z;
+        plane_2d.push_back( Point( point.dot(X), point.dot(Y) ) );
+    }
+
+    for(auto p : boundary->points){
+        point[0] = p.x;
+        point[1] = p.y;
+        point[2] = p.z;
+        boundary_2d.push_back( Point( point.dot(X), point.dot(Y) ) );
+    }
 }
 
 
@@ -76,24 +121,28 @@ void constrainedDelaunayTriangulation(  std::vector<Point>                      
 
     CDT cdt;
 
+    if(points.size() < 3) return;
+    int size = points.size();
+    int points_size = size;
+
     // Insert the points.
     insert_with_info(cdt, points.begin(),points.end());
 
     // Insert the constraints (boundary).
-    for (size_t i = 0; i < constraines.size()-1; i++) {
-        cdt.insert_constraint(constraines[i], constraines[i+1]);
-    }
+    if(constraines.size() > 1){
+        for (size_t i = 0; i < constraines.size()-1; i++) {
+            cdt.insert_constraint(constraines[i], constraines[i+1]);
+        }
 
-    // put the correct index on the constraine points.
-    auto it = cdt.vertices_begin();
-    int size = points.size();
-    int points_size = size;
-    std::cout << "size: " << size << std::endl;
-    std::advance(it, size);
-    for(; it != cdt.vertices_end(); ++it){
-        it->info() = size;
-        std::cout << size << std::endl;
-        size++;
+        // put the correct index on the constraine points.
+        auto it = cdt.vertices_begin();
+        std::cout << "size: " << size << std::endl;
+        std::advance(it, size);
+        for(; it != cdt.vertices_end(); ++it){
+            it->info() = size;
+            std::cout << size << std::endl;
+            size++;
+        }
     }
 
     // Clear Idx if not empty and reserve approximate size;
--- test/planeDetectionComparison/src/CGALtest.cpp
+++ test/planeDetectionComparison/src/CGALtest.cpp
@@ -1,5 +1,12 @@
 #include <ros/ros.h>
-
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/PolygonMesh.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/conversions.h>
+#include <pcl/for_each_type.h>
+#include <pcl/point_traits.h>
+#include <pcl/visualization/cloud_viewer.h>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
@@ -8,6 +15,7 @@
 #include <CGAL/Spatial_sort_traits_adapter_2.h>
 #include <planeDetectionComparison/cgalTriangulation.h>
 
+#include <pcl_conversions/pcl_conversions.h>
 #include <vector>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel            Kernel;
@@ -22,59 +30,48 @@ typedef CGAL::Exact_predicates_tag                                  Itag;
 typedef CGAL::Constrained_Delaunay_triangulation_2<Kernel, TDS, Itag>    CDT;
 typedef CGAL::Spatial_sort_traits_adapter_2<Kernel,Point*> Search_traits;
 
-// template <class InputIterator>
-// void insert_with_info(CDT& cdt, InputIterator first,InputIterator last)
-// {
-//   std::vector<std::ptrdiff_t> indices;
-//   std::vector<Point> points;
-//   std::ptrdiff_t index=0;
-//
-//   for (InputIterator it=first;it!=last;++it){
-//     points.push_back( *it);
-//     indices.push_back(index++);
-//   }
-//
-//   CGAL::spatial_sort(indices.begin(),indices.end(),Search_traits(&(points[0]),cdt.geom_traits()));
-//
-//   CDT::Vertex_handle v_hint;
-//   CDT::Face_handle hint;
-//   for (typename std::vector<std::ptrdiff_t>::const_iterator
-//     it = indices.begin(), end = indices.end();
-//     it != end; ++it){
-//     v_hint = cdt.insert(points[*it], hint);
-//     if (v_hint!=CDT::Vertex_handle()){
-//       v_hint->info()=*it;
-//       hint=v_hint->face();
-//     }
-//   }
-// }
+using namespace std;
 
-// template <class InputIterator>
-// void insert_constraint_with_info(CDT& cdt, InputIterator first,InputIterator last,int idx)
-// {
-//   std::vector<std::ptrdiff_t> indices;
-//   std::vector<Point> points;
-//   std::ptrdiff_t index=0;
-//
-//   for (InputIterator it=first;it!=last;++it){
-//     points.push_back( *it);
-//     indices.push_back(index++);
-//   }
-//
-//   CGAL::spatial_sort(indices.begin(),indices.end(),Search_traits(&(points[0]),cdt.geom_traits()));
-//
-//   CDT::Vertex_handle v_hint;
-//   CDT::Face_handle hint;
-//   for (typename std::vector<std::ptrdiff_t>::const_iterator
-//     it = indices.begin(), end = indices.end();
-//     it != end; ++it){
-//     v_hint = cdt.insert_constraint(points[*it], hint);
-//     if (v_hint!=CDT::Vertex_handle()){
-//       v_hint->info()=*it+idx;
-//       hint=v_hint->face();
-//     }
-//   }
-// }
+float createPlaneZ(float x, float y){
+    float a,b,c,d;
+    a = 1;
+    b = 2;
+    c = 3;
+    d = 2;
+    return -(a*x+b*y+d)/c;
+}
+
+template<typename PointT> void
+toMeshCloud (const pcl::PointCloud<PointT>& cloud, pcl::PCLPointCloud2& msg)
+{
+    // Ease the user's burden on specifying width/height for unorganized datasets
+    if (cloud.width == 0 && cloud.height == 0)
+    {
+      msg.width  = static_cast<uint32_t>(cloud.points.size ());
+      msg.height = 1;
+    }
+    else
+    {
+      assert (cloud.points.size () == cloud.width * cloud.height);
+      msg.height = cloud.height;
+      msg.width  = cloud.width;
+    }
+
+    // Fill point cloud binary data (padding and all)
+    size_t data_size = sizeof (PointT) * cloud.points.size ();
+    msg.data.resize (data_size);
+    memcpy (&msg.data[0], &cloud.points[0], data_size);
+
+    // Fill fields metadata
+    msg.fields.clear ();
+    pcl::for_each_type<typename pcl::traits::fieldList<PointT>::type> (pcl::detail::FieldAdder<PointT>(msg.fields));
+
+    msg.header     = cloud.header;
+    msg.point_step = sizeof (PointT);
+    msg.row_step   = static_cast<uint32_t> (sizeof (PointT) * msg.width);
+    msg.is_dense   = cloud.is_dense;
+    /// @todo msg.is_bigendian = ?;
+}
 
 int main(int argc, char **argv) {
 //
@@ -82,76 +79,93 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh = ros::NodeHandle("~");
     ros::Rate loop_rate(10);
 
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr boundary (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::ModelCoefficients::Ptr coeff (new pcl::ModelCoefficients());
+    coeff->values.resize(3);
+    coeff->values[0] = 1;
+    coeff->values[1] = 2;
+    coeff->values[2] = 3;
+
+    std::vector<Point> plane_2d;
+    std::vector<Point> boundary_2d;
+
+    pcl::PointXYZ p;
+    for (size_t i = 0; i < 10; i++) {
+        for (size_t j = 0; j < 10; j++) {
+            p.x = i;
+            p.y = j;
+            p.z = createPlaneZ(i,j);
+            cloud->points.push_back(p);
+        }
+    }
+
+    pclPlaneToCGAL<pcl::PointXYZ>(cloud, boundary, coeff, plane_2d, boundary_2d);
+    std::vector<std::vector<unsigned int> > idx;
+    std::cout << "hmmmmm" << std::endl;
+    constrainedDelaunayTriangulation(plane_2d, boundary_2d, idx);
+
+    pcl::PolygonMesh mesh;
+    toMeshCloud(*cloud, mesh.cloud);
+
+
+    pcl::Vertices vert;
+    vert.vertices.resize(3);
+    for(auto poly : idx){
+        vert.vertices[0] = poly[0];
+        vert.vertices[1] = poly[1];
+        vert.vertices[2] = poly[2];
+        mesh.polygons.push_back(vert);
+        std::cout << poly[0] << ", " << poly[1] << ", " << poly[2] << std::endl;
+        std::cout << "-----------------------" << std::endl;
+    }
+
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    viewer->setBackgroundColor (0, 0, 0);
+    viewer->addPolygonMesh(mesh,"meshes",0);
+    viewer->addCoordinateSystem (1.0);
+    viewer->initCameraParameters ();
+    while (!viewer->wasStopped ()){s
+        viewer->spinOnce (100);
+        boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+    }
 
 
 
-  std::vector<Point> points;
-  points.push_back(Point(1,1));
-  points.push_back(Point(2,1));
-  points.push_back(Point(3,1));
-  points.push_back(Point(4,1));
-  points.push_back(Point(5,1));
-  points.push_back(Point(1,2));
-  points.push_back(Point(2,2));
-  points.push_back(Point(3,2));
-  points.push_back(Point(4,2));
-  points.push_back(Point(5,2));
-  points.push_back(Point(1,3));
-  points.push_back(Point(2,3));
-  points.push_back(Point(1,4));
-  points.push_back(Point(2,4));
-  points.push_back(Point(4,3));
-  points.push_back(Point(5,3));
-  points.push_back(Point(4,4));
-  points.push_back(Point(5,4));
-
-  std::vector<Point> constrained;
-  constrained.push_back(Point(2.5,4));
-  constrained.push_back(Point(2.5,3));
-  constrained.push_back(Point(3,3));
-  constrained.push_back(Point(3.5,3));
-  constrained.push_back(Point(3.5,4));
-
-  std::vector<std::vector<unsigned int> > idx;
-  constrainedDelaunayTriangulation(points, constrained, idx);
-
-
-  for(auto poly : idx){
-      std::cout << poly[0] << ", " << poly[1] << ", " << poly[2] << std::endl;
-      std::cout << "-----------------------" << std::endl;
-  }
-
-  // Delaunay triangulation;
-  // triangulation.insert(points.begin(),points.end());
+  // std::vector<Point> points;
+  // points.push_back(Point(1,1));
+  // points.push_back(Point(2,1));
+  // points.push_back(Point(3,1));
+  // points.push_back(Point(4,1));
+  // points.push_back(Point(5,1));
+  // points.push_back(Point(1,2));
+  // points.push_back(Point(2,2));
+  // points.push_back(Point(3,2));
+  // points.push_back(Point(4,2));
+  // points.push_back(Point(5,2));
+  // points.push_back(Point(1,3));
+  // points.push_back(Point(2,3));
+  // points.push_back(Point(1,4));
+  // points.push_back(Point(2,4));
+  // points.push_back(Point(4,3));
+  // points.push_back(Point(5,3));
+  // points.push_back(Point(4,4));
+  // points.push_back(Point(5,4));
   //
-  // CDT cdt;
-  // CDT::Vertex_handle verttest;
-  // insert_with_info(cdt, points.begin(),points.end());
-  // for (size_t i = 0; i < constrained.size()-1; i++) {
-  //     cdt.insert_constraint(constrained[i], constrained[i+1]);
-  // }
+  // std::vector<Point> constrained;
+  // constrained.push_back(Point(2.5,4));
+  // constrained.push_back(Point(2.5,3));
+  // constrained.push_back(Point(3,3));
+  // constrained.push_back(Point(3.5,3));
+  // constrained.push_back(Point(3.5,4));
   //
-  // auto it = cdt.vertices_begin();
-  // std::advance(it, points.size());
-  // int size = points.size();
-  // for(; it != cdt.vertices_end(); ++it){
-  //     it->info() = size;
-  //     std::cout << it->info() << std::endl;
-  //     size++;
-  // }
-  //
-  // // insert_constraint_with_info(cdt, constrained.begin(), constrained.end(), points.size());
-  // // cdt.insert_constraint(Point(2.5, 0), Point(2.5,2));
-  //
-  // for(CDT::Finite_faces_iterator fit = cdt.finite_faces_begin();
-  //     fit != cdt.finite_faces_end(); ++fit) {
-  //
-  //   CDT::Face_handle face = fit;
-  //   // std::cout << "Triangle:\t" << cdt.triangle(face) << std::endl;
-  //   std::cout << "Vertex 0:\t" << face->vertex(0)->info() << ", ";
-  //   std::cout << face->vertex(1)->info() << ", ";
-  //   std::cout << face->vertex(2)->info() << std::endl;
-  //   std::cout << "----------------------------" << std::endl;
+  // std::vector<std::vector<unsigned int> > idx;
+  // constrainedDelaunayTriangulation(points, constrained, idx);
+
+
+  // for(auto poly : idx){
+  //     std::cout << poly[0] << ", " << poly[1] << ", " << poly[2] << std::endl;
+  //     std::cout << "-----------------------" << std::endl;
   // }
 
 
